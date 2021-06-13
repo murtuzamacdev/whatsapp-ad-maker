819427f19531fd2120faf708c4cb7e4ea9aea672
Murtuza Chakkiwala <>
2021-06-13 11:00:19 +0530

    tempalte 1

--- src/templates/template1/Template1.jsx
+++ src/templates/template1/Template1.jsx
@@ -1,24 +1,28 @@
 import './Template1.scss';
+import currencies from '../../configs/currencies.json';
 
 // Assets
 import whatsappLogo from '../../assets/images/logos_whatsapp.png';
 
 const Template1 = ({ productData }) => {
-    return (<div style={{ backgroundImage: "url(" + productData.productImage + ")" }} className="temp1-ctnr d-flex flex-column justify-content-between align-items-center ">
-        <div className="mt-3"><div className="product-price pt-2 pb-2 pl-4 pr-4">₹ 22000.00</div></div>
+    const productDescDivHeight = window.innerHeight * (122 / 812);
+
+    return (<div style={{ backgroundImage: "url(" + productData.productImage + ")" }} className="temp1-ctnr">
+        {productData.productPrice !== '' && <div className="mt-3 d-flex justify-content-center single-line"><div className="product-price pt-2 pb-2 pl-4 pr-4"><small>{currencies.find((item) => item.code === productData.currencyCode).symbol}</small> {productData.productPrice}<small>.00</small></div></div>}
         <div className="mb-3 bottom-ctrn">
-            <p className="product-name pt-2 pb-2 pl-1 pr-1">Product </p>
+            <p className="product-name pt-2 pb-2 pl-1 pr-1 m-0">{productData.productName}</p>
             <div className="purple-ctnr pt-4 pb-4 pl-3 pr-3">
-                <div className="product-desc mt-4 mb-3 pl-2">100% Original item. 100% Original item.</div>
+                {productData.productDescription !== '' && <div className="product-desc mt-4 mb-3 pl-2" style={{ maxHeight: `${productDescDivHeight}px` }}>{productData.productDescription}</div>}
                 <div className="d-flex justify-content-between align-items-center">
                     <div className="order-now pl-2 pr-2">Order Now</div>
-                    <div>
-                        <div className='seller-name'>Your Brand Name</div>
-                        <div className="d-flex mt-1">
+                    {(productData.sellerName !== '' || productData.whatsappNumber !== '') && <div className="seller-info-ctrn">
+                        {productData.sellerName !== '' && <div className='seller-name single-line'>{productData.sellerName}</div>}
+                        {productData.whatsappNumber !== '' && <div className="d-flex mt-1">
                             <img src={whatsappLogo} height="24px" width="24px" alt="whatsappLogo"></img>
-                            <p className="whatsapp-number m-0 ml-2">1234567890</p>
-                        </div>
-                    </div>
+                            <p className="whatsapp-number m-0 ml-2">{productData.whatsappNumber}</p>
+                        </div>}
+                    </div>}
+
                 </div>
             </div>
 
