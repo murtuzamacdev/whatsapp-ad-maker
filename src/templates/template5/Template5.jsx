0c12ee19a116e3e0dce99962a27ef2ec18832619
Murtuza Chakkiwala <>
2021-06-21 18:34:44 +0530

    css updates

--- src/templates/template5/Template5.jsx
+++ src/templates/template5/Template5.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import './Template5.scss';
 import currencies from '../../configs/currencies.json';
 import { GlobalContext } from '../../context/global.context';
+import {hexToRgbA} from '../../utility';
 
 // Assets
 import backdrop from '../../assets/images/backdrop.png';
@@ -11,21 +12,21 @@ const Template5 = () => {
     const globalContext = useContext(GlobalContext);
     const productDescDivHeight = window.innerHeight * (122 / 812);
 
-    function hexToRgbA() {
-        var c;
-        let hex = globalContext.state.productData.selectedBackgroundColor
-        if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
-            c = hex.substring(1).split('');
-            if (c.length === 3) {
-                c = [c[0], c[0], c[1], c[1], c[2], c[2]];
-            }
-            c = '0x' + c.join('');
-            return 'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',0.65)';
-        }
-        throw new Error('Bad Hex');
-    }
+    // function hexToRgbA() {
+    //     var c;
+    //     let hex = globalContext.state.productData.selectedBackgroundColor
+    //     if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+    //         c = hex.substring(1).split('');
+    //         if (c.length === 3) {
+    //             c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+    //         }
+    //         c = '0x' + c.join('');
+    //         return 'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',0.65)';
+    //     }
+    //     throw new Error('Bad Hex');
+    // }
 
-    return (<div class="template5-cntr" style={{ background: 'linear-gradient( 0.74deg , ' +  hexToRgbA() + ' 0.59%, ' + globalContext.state.productData.selectedBackgroundColor + ' 99.01%)' }}>
+    return (<div class="template5-cntr" style={{ background: 'linear-gradient( 0.74deg , ' + globalContext.state.productData.selectedBackgroundColor  + ' 0.59%, ' + hexToRgbA(globalContext.state.productData.selectedBackgroundColor, 0.7)  + ' 99.01%)' }}>
         <div className="card p-0 main-card">
             {/* Main Image */}
             <div style={{ minHeight: '60%', flexGrow: 1 }} className="d-flex product-image-card justify-content-center p-0">
@@ -33,7 +34,7 @@ const Template5 = () => {
                 <img className="product-image-backdrop" src={backdrop} alt="productBackdrop" />
                 <div className="d-flex name-price-ctnr product-name-card justify-content-between align-items-center flex-column">
                     <p style={{ flexGrow: 0.7 }} className="m-0 product-name">{globalContext.state.productData.productName}</p>
-                    {globalContext.state.productData.productPrice !== '' && <p className="m-0 product-price pt-3 pb-2 pl-4 pr-4" style={{ backgroundColor: globalContext.state.productData.selectedBackgroundColor }}><small>{currencies.find((item) => item.code === globalContext.state.productData.currencyCode).symbol}</small> {globalContext.state.productData.productPrice}</p>}
+                    {globalContext.state.productData.productPrice !== '' && <p className="m-0 product-price pt-1 pb-2 pl-4 pr-4" style={{ backgroundColor: globalContext.state.productData.selectedBackgroundColor }}><small>{currencies.find((item) => item.code === globalContext.state.productData.currencyCode).symbol}</small> {globalContext.state.productData.productPrice}</p>}
                 </div>
 
             </div>
