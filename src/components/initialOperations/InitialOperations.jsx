3be45a825d1559371b57dd987b53660b9aa60046
Murtuza Chakkiwala <>
2021-06-24 15:42:40 +0530

    added dev and prod environment

--- src/components/initialOperations/InitialOperations.jsx
+++ src/components/initialOperations/InitialOperations.jsx
@@ -27,14 +27,14 @@ const InitialOperations = () => {
 
     const initFirebase = () => {
         var firebaseConfig = {
-            apiKey: "AIzaSyAnsFuEK5xZ_1a30quyudtw7ILmroM5rfA",
-            authDomain: "dev-createawesomeads.firebaseapp.com",
-            databaseURL: "https://dev-createawesomeads-default-rtdb.asia-southeast1.firebasedatabase.app",
-            projectId: "dev-createawesomeads",
-            storageBucket: "dev-createawesomeads.appspot.com",
-            messagingSenderId: "1040397287421",
-            appId: "1:1040397287421:web:3d7924f947eda51317a394",
-            measurementId: "G-PECZRHLJ71"
+            apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+            authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+            databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+            projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+            storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+            appId: process.env.REACT_APP_FIREBASE_APP_ID,
+            measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
         };
 
         // Initialize Firebase
