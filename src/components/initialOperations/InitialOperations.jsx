5e0798770126ef59209420d9bd3b53e89761492e
Murtuza Chakkiwala <>
2021-06-24 19:03:05 +0530

    just checking

--- src/components/initialOperations/InitialOperations.jsx
+++ src/components/initialOperations/InitialOperations.jsx
@@ -26,7 +26,7 @@ const InitialOperations = () => {
     }, [])
 
     const initFirebase = () => {
-        var firebaseConfig = {
+        var firebaseConfig = { 
             apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
             authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
             databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
