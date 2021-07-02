844eccb504f11829b0d559701eae1fd43c3a2cfe
Murtuza Chakkiwala <>
2021-07-02 15:46:16 +0530

    prod and dev unsplash api key

--- src/configs/constants.js
+++ src/configs/constants.js
@@ -1,4 +1,4 @@
-export const UNSPLASH_API_KEY = '3Aw5X4DAYM5iT7MXgtQ-L6oUfnWUjgoyoZDGnyaWjIQ';
+export const UNSPLASH_API_KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
 export const UNSPLASH_APP_NAME = 'Create Awesome Ads'
 
 export const PITCH_DEFAULT = ['ORDER NOW!', 'HURRY UP!', 'SALE!!!', '50% OFF', '1 + 1 FREE', 'CALL NOW!'];
